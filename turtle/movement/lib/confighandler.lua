ec98ab6bf811a513e377b48c7da527a3b977dfc9
Rafael Ferreira de Castilho Silva <58736323+RafaelferreiraD@users.noreply.github.com>
2024-10-26 02:46:09 -0300

    Changes to be committed: 	modified:   turtle/movement/lib/confighandler.lua

--- turtle/movement/lib/confighandler.lua
+++ turtle/movement/lib/confighandler.lua
@@ -4,7 +4,7 @@ require "lib.stringhandler"
 function Readconf(file)
     local lines = Readlines(file)
     local line = {}
-    local index = 0
+    local index = tonumber(0)
     local conf = {
         ---        {
         ---            ["from_to_str"] = nil,
@@ -17,8 +17,8 @@ function Readconf(file)
         if not string.find(lines[i], "---") then
             line = Split(lines[i], "=")
             if string.find(line[1], "index") then
-                index = line[2]
-                conf[tonumber(index)] = {}
+                index = tonumber(line[2])
+                conf[index] = {}
                 conf[index]["step"] = {}
             end
             if string.find(line[1], "step") then
