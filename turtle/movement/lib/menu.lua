90159e70c269346b217b18ebfd76be1a5867a587
Rafael Ferreira de Castilho Silva <58736323+RafaelferreiraD@users.noreply.github.com>
2024-10-26 02:36:51 -0300

    Changes to be committed: 	modified:   turtle/movement/lib/menu.lua

--- turtle/movement/lib/menu.lua
+++ turtle/movement/lib/menu.lua
@@ -3,6 +3,8 @@ require "lib.confighandler"
 function Showmenu(file)
     local conf = Readconf(file)
     print("Press any key to continue:")
+    print(conf)
+    print(conf[1]["index"])
 
     for i = 1, #conf, 1 do
         print("[" .. i .. "]" .. "- Route name: " .. conf[i]["from_to_str"] .. " - Setpu block" .. conf[i]["setup_str"])
